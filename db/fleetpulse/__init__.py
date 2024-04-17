0a702b4ac9737d0fd1d5b60e7d19ed1de3aee3e6
ctlbau <ctrebbau@pm.me>
2024-04-17 11:43:58 +0200

    Fixes import errors

--- db/fleetpulse/__init__.py
+++ db/fleetpulse/__init__.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 from .db_support import fetch_managers, fetch_centers, fetch_statuses, fetch_date_range, fetch_plates, fetch_vehicles, select_plate, get_vehicle_stati
-=======
-from .db_support import fetch_managers, fetch_centers, fetch_date_range, fetch_plates, fetch_statuses, fetch_vehicles
->>>>>>> shift-holes
