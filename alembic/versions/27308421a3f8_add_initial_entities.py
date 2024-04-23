efb6ba0926e442a2e4c0200f26c4d3e1ff16bc82
ctlbau <ctrebbau@pm.me>
2024-04-23 17:11:13 +0200

    Conditions alembic table creations to not already existing in db

--- alembic/versions/27308421a3f8_add_initial_entities.py
+++ alembic/versions/27308421a3f8_add_initial_entities.py
@@ -9,7 +9,7 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
-
+from sqlalchemy.engine.reflection import Inspector
 
 # revision identifiers, used by Alembic.
 revision: str = '27308421a3f8'
@@ -19,53 +19,68 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_table('Managers',
-                    sa.Column('id', sa.Integer, primary_key=True),
-                    sa.Column('name', sa.String(50), nullable=False))
+    conn = op.get_bind()
+    insp = Inspector.from_engine(conn)
+    tables = insp.get_table_names()
+    
+    if 'Managers' not in tables:
+        op.create_table('Managers',
+                        sa.Column('id', sa.Integer, primary_key=True),
+                        sa.Column('name', sa.String(50), nullable=False),
+                        sa.Index('ixd_managers_name', 'name'))
 
-    op.create_table('Shifts',
-                    sa.Column('id', sa.Integer, primary_key=True),
-                    sa.Column('name', sa.String(50), nullable=False))
+    if 'Shifts' not in tables:
+        op.create_table('Shifts',
+                        sa.Column('id', sa.Integer, primary_key=True),
+                        sa.Column('name', sa.String(50), nullable=False))
 
-    op.create_table('Companies',
-                    sa.Column('id', sa.Integer, primary_key=True),
-                    sa.Column('name', sa.String(60), nullable=False))
+    if 'Companies' not in tables:
+        op.create_table('Companies',
+                        sa.Column('id', sa.Integer, primary_key=True),
+                        sa.Column('name', sa.String(60), nullable=False))
 
-    op.create_table('Centers',
-                    sa.Column('id', sa.Integer, primary_key=True),
-                    sa.Column('name', sa.String(70), nullable=False))
+    if 'Centers' not in tables:
+        op.create_table('Centers',
+                        sa.Column('id', sa.Integer, primary_key=True),
+                        sa.Column('name', sa.String(70), nullable=False),
+                        sa.Index('ixd_centers_name', 'name'))
 
-    op.create_table('Provinces',
-                    sa.Column('id', sa.Integer, primary_key=True),
-                    sa.Column('name', sa.String(70), nullable=False))
+    if 'Provinces' not in tables:
+        op.create_table('Provinces',
+                        sa.Column('id', sa.Integer, primary_key=True),
+                        sa.Column('name', sa.String(70), nullable=False))
 
-    op.create_table('ExchangeLocations',
-                    sa.Column('id', sa.Integer, autoincrement=True, primary_key=True),
-                    sa.Column('name', sa.String(70), nullable=False, unique=True))
+    if 'ExchangeLocations' not in tables:
+        op.create_table('ExchangeLocations',
+                        sa.Column('id', sa.Integer, autoincrement=True, primary_key=True),
+                        sa.Column('name', sa.String(70), nullable=False, unique=True))
 
-    op.create_table('Drivers',
-                    sa.Column('kendra_id', sa.Integer, primary_key=True),
-                    sa.Column('name', sa.String(80), nullable=False),
-                    sa.Column('street', sa.String(255), nullable=True),
-                    sa.Column('city', sa.String(70), nullable=True),
-                    sa.Column('country', sa.String(70), nullable=True),
-                    sa.Column('zip_code', sa.String(20), nullable=True),
-                    sa.Column('lat', sa.Float, nullable=True),
-                    sa.Column('lng', sa.Float, nullable=True),
-                    sa.Column('province_id', sa.Integer, sa.ForeignKey('Provinces.id'), nullable=True),
-                    sa.Column('manager_id', sa.Integer, sa.ForeignKey('Managers.id'), nullable=True),
-                    sa.Column('shift_id', sa.Integer, sa.ForeignKey('Shifts.id'), nullable=True))
+    if 'Drivers' not in tables:
+        op.create_table('Drivers',
+                        sa.Column('kendra_id', sa.Integer, primary_key=True),
+                        sa.Column('name', sa.String(80), nullable=False),
+                        sa.Column('street', sa.String(255), nullable=True),
+                        sa.Column('city', sa.String(70), nullable=True),
+                        sa.Column('country', sa.String(70), nullable=True),
+                        sa.Column('zip_code', sa.String(20), nullable=True),
+                        sa.Column('lat', sa.Float, nullable=True),
+                        sa.Column('lng', sa.Float, nullable=True),
+                        sa.Column('province_id', sa.Integer, sa.ForeignKey('Provinces.id'), nullable=True),
+                        sa.Column('manager_id', sa.Integer, sa.ForeignKey('Managers.id'), nullable=True),
+                        sa.Column('shift_id', sa.Integer, sa.ForeignKey('Shifts.id'), nullable=True))
 
-    op.create_table('Vehicles',
-                    sa.Column('kendra_id', sa.Integer, nullable=False),
-                    sa.Column('plate', sa.String(10), nullable=False),
-                    sa.Column('status', sa.String(17), nullable=False),
-                    sa.Column('date', sa.Date, nullable=False),
-                    sa.Column('company_id', sa.Integer, sa.ForeignKey('Companies.id'), nullable=False),
-                    sa.Column('center_id', sa.Integer, sa.ForeignKey('Centers.id'), nullable=False),
-                    sa.Column('manager_id', sa.Integer, sa.ForeignKey('Managers.id'), nullable=True),
-                    sa.PrimaryKeyConstraint('date', 'plate'),
-                    sa.Index('idx_kendra_id', 'kendra_id'))
+    if 'Vehicles' not in tables:
+        op.create_table('Vehicles',
+                        sa.Column('kendra_id', sa.Integer, nullable=False),
+                        sa.Column('plate', sa.String(10), nullable=False),
+                        sa.Column('status', sa.String(17), nullable=False),
+                        sa.Column('date', sa.Date, nullable=False),
+                        sa.Column('company_id', sa.Integer, sa.ForeignKey('Companies.id'), nullable=False),
+                        sa.Column('center_id', sa.Integer, sa.ForeignKey('Centers.id'), nullable=False),
+                        sa.Column('manager_id', sa.Integer, sa.ForeignKey('Managers.id'), nullable=True),
+                        sa.PrimaryKeyConstraint('date', 'plate'),
+                        sa.Index('ixd_vehicles_plate', 'plate'),
+                        sa.Index('idx_kendra_id', 'kendra_id'))
 
 def downgrade() -> None:
     op.drop_table('Vehicles')
--- alembic/versions/c489041f33b3_create_join_tables_for_companies_.py
+++ alembic/versions/c489041f33b3_create_join_tables_for_companies_.py
@@ -9,7 +9,7 @@ from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
-
+from sqlalchemy.engine.reflection import Inspector
 
 # revision identifiers, used by Alembic.
 revision: str = 'c489041f33b3'
@@ -19,22 +19,29 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_table('CompaniesCenters',
-                    sa.Column('company_id', sa.Integer, nullable=False),
-                    sa.Column('center_id', sa.Integer, nullable=False),
-                    sa.PrimaryKeyConstraint('company_id', 'center_id'),
-                    sa.ForeignKeyConstraint(['company_id'], ['Companies.id'], ondelete='CASCADE'),
-                    sa.ForeignKeyConstraint(['center_id'], ['Centers.id'], ondelete='CASCADE')
-                    )
-    op.create_table('DriversVehiclesExchangeLocations',
-                    sa.Column('driver_id', sa.Integer, nullable=False),
-                    sa.Column('vehicle_id', sa.Integer, nullable=False),
-                    sa.Column('exchange_location_id', sa.Integer, nullable=True),
-                    sa.PrimaryKeyConstraint('driver_id', 'vehicle_id', 'exchange_location_id'),
-                    sa.ForeignKeyConstraint(['driver_id'], ['Drivers.kendra_id'], ondelete='CASCADE'),
-                    sa.ForeignKeyConstraint(['vehicle_id'], ['Vehicles.kendra_id'], ondelete='CASCADE'),
-                    sa.ForeignKeyConstraint(['exchange_location_id'], ['ExchangeLocations.id'])
-                    )
+    conn = op.get_bind()
+    insp = Inspector.from_engine(conn)
+    tables = insp.get_table_names()
+    
+    if 'CompaniesCenters' not in tables:
+        op.create_table('CompaniesCenters',
+                        sa.Column('company_id', sa.Integer, nullable=False),
+                        sa.Column('center_id', sa.Integer, nullable=False),
+                        sa.PrimaryKeyConstraint('company_id', 'center_id'),
+                        sa.ForeignKeyConstraint(['company_id'], ['Companies.id'], ondelete='CASCADE'),
+                        sa.ForeignKeyConstraint(['center_id'], ['Centers.id'], ondelete='CASCADE')
+                        )
+        
+    if 'DriversVehiclesExchangeLocations' not in tables:
+        op.create_table('DriversVehiclesExchangeLocations',
+                        sa.Column('driver_id', sa.Integer, nullable=False),
+                        sa.Column('vehicle_id', sa.Integer, nullable=False),
+                        sa.Column('exchange_location_id', sa.Integer, nullable=True),
+                        sa.PrimaryKeyConstraint('driver_id', 'vehicle_id', 'exchange_location_id'),
+                        sa.ForeignKeyConstraint(['driver_id'], ['Drivers.kendra_id'], ondelete='CASCADE'),
+                        sa.ForeignKeyConstraint(['vehicle_id'], ['Vehicles.kendra_id'], ondelete='CASCADE'),
+                        sa.ForeignKeyConstraint(['exchange_location_id'], ['ExchangeLocations.id'])
+                        )
 
 def downgrade() -> None:
     op.drop_table('DriversVehiclesExchangeLocations')
