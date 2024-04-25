d20aa1c02e122374e0fb79fd902454af8ac148cf
ctlbau <ctrebbau@pm.me>
2024-04-25 10:27:39 +0200

    Historical tables for vehicle shift holes and distance manager stats set up via alembic. Helper functions aligned to produce data according to the schema of these tables. Scripts to be set up as cronjobs ready

--- db/crons/vehicles_shifts_snapshot.py
+++ db/crons/vehicles_shifts_snapshot.py
@@ -4,7 +4,7 @@ from ..db_connect import localauth_dev, localauth_stg, localauth_prod, companies
 
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
-from db.blockbuster.db_support import fetch_vehicle_shifts
+from db.assignments.db_support import create_vehicle_shifts
 
 from datetime import datetime
 from dotenv import load_dotenv
@@ -25,37 +25,36 @@ else:
 
 
 def fetch_prepare_and_insert_vehicle_shifts_snapshot():
-    shifts = fetch_vehicle_shifts()
-    required_columns = ['plate', 'manana', 'tarde', 'l_j_40h', 'tp_v_d', 'number_of_drivers', 'manager', 'center']
-    shifts.rename(columns={
-        'Mañana': 'manana',
-        'Tarde': 'tarde',
-        'L-J_(40h)': 'l_j_40h',
-        'TP-V-D': 'tp_v_d'
-    }, inplace=True)
-    shifts = shifts[required_columns]
+    shifts = create_vehicle_shifts()
+    
     shifts['date'] = datetime.today().date()
     shifts['manana'] = shifts['manana'].astype(int)
     shifts['tarde'] = shifts['tarde'].astype(int)
     shifts['l_j_40h'] = shifts['l_j_40h'].astype(int)
     shifts['tp_v_d'] = shifts['tp_v_d'].astype(int)
+    shifts['tp_l_v'] = shifts['tp_l_v'].astype(int)
+    shifts['l_j'] = shifts['l_j'].astype(int)
+    shifts['turno_completo'] = shifts['turno_completo'].astype(int)
     shifts['number_of_drivers'] = shifts['number_of_drivers'].astype(int)
     
     engine = connect(localauth)
 
     sql = text("""
-    INSERT INTO VehicleShiftHistorical
-        (date, plate, manager, center, number_of_drivers, manana, tarde, l_j_40h, tp_v_d)
+    INSERT INTO VehicleShiftsHistorical
+        (date, plate, manager, center, number_of_drivers, manana, tarde, tp_v_d, tp_l_v, l_j, l_j_40h, turno_completo)
     VALUES
-        (:date, :plate, :manager, :center, :number_of_drivers, :manana, :tarde, :l_j_40h, :tp_v_d)
+        (:date, :plate, :manager, :center, :number_of_drivers, :manana, :tarde, :tp_v_d, :tp_l_v, :l_j, :l_j_40h, :turno_completo)
     ON DUPLICATE KEY UPDATE
         manager = VALUES(manager),
         center = VALUES(center),
         number_of_drivers = VALUES(number_of_drivers),
         manana = VALUES(manana),
+        tp_v_d = VALUES(tp_v_d),
         tarde = VALUES(tarde),
+        tp_l_v = VALUES(tp_l_v),
+        l_j = VALUES(l_j),
         l_j_40h = VALUES(l_j_40h),
-        tp_v_d = VALUES(tp_v_d);
+        turno_completo = VALUES(turno_completo);
     """)
 
     try:
@@ -65,7 +64,7 @@ def fetch_prepare_and_insert_vehicle_shifts_snapshot():
                 for index, row in shifts.iterrows():
                     conn.execute(sql, row.to_dict())
                 transaction.commit()
-                print(f"Inserted {len(shifts)} rows into VehicleShiftHistorical table")
+                print(f"Inserted {len(shifts)} rows into VehicleShiftsHistorical table")
             except Exception as e:
                 transaction.rollback()
                 print("Transaction failed and was rolled back.")
