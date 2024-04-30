6d91e7adddb384fbe83724e1fc0e483a354f4898
ctlbau <ctrebbau@pm.me>
2024-04-30 12:50:29 +0200

    fixes minor refactoring map chosen style bug

--- ui/components.py
+++ ui/components.py
@@ -11,14 +11,8 @@ import os
 
 ATOCHA = (-3.690633, 40.406785)
 MAP_STYLES = ["mapbox://styles/mapbox/light-v9", "mapbox://styles/mapbox/dark-v9", "mapbox://styles/mapbox/satellite-v9"]
-# CHOSEN_STYLE = MAP_STYLES[0]
 MAPBOX_API_KEY = os.getenv("MAPBOX_TOKEN")
-<<<<<<< HEAD
-
-def create_map_container(id, initial_view_coords=ATOCHA, tooltip_info={}, map_style=CHOSEN_STYLE):
-=======
 def create_map_container(id, initial_view_coords=ATOCHA, tooltip_info={}, map_style=MAP_STYLES[0]):
->>>>>>> historical-shift
         return dcc.Loading(
                 id="loading-map", 
                 children=[
