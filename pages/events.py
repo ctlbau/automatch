6a8374cfccdf81cc01e56f250a45f882a00da4f7
ctlbau <ctrebbau@pm.me>
2024-06-13 16:45:08 +0200

    Changes visual display to horizontal bars of manager and driver view of events

--- pages/events.py
+++ pages/events.py
@@ -11,7 +11,7 @@ from utils.agg_utils import expand_events
 dash.register_page(__name__, path='/events')
 
 layout = html.Div([
-    dcc.Tabs(id="event-tabs", value='driver-event-tab', children=[
+    dcc.Tabs(id="event-tabs", value='manager-event-tab', children=[
         dcc.Tab(label='Manager View', value='manager-event-tab'),
         dcc.Tab(label='Driver View', value='driver-event-tab'),
     ], className="col-md-3 offset-md-1 col-12"),
@@ -25,13 +25,22 @@ def render_content(tab):
         min_date, max_date = get_min_max_dates_from_schedule_events()
         manager_options = fetch_managers().to_dict('records')
         event_options = fetch_event_options().to_dict('records')
-        event_options = [event for event in event_options if event['name'] not in ['Libre', 'Alta Medica']]
+        # event_options = [event for event in event_options if event['name'] not in ['Libre', 'Alta Medica']]
         manager_layout = dbc.Container([
             dbc.Row([
                 dbc.Col([
                     create_date_range_picker('event-date-range-picker', min_date, max_date),
                     create_dropdown('manager-dropdown', options=manager_options, label='name', value='name', placeholder='Select manager', multi=True, add_all=True),
                     create_dropdown('event-dropdown', options=event_options, label='name', value='name', placeholder='Select event', multi=True, add_all=False),
+                    dcc.RadioItems(
+                        id='scale-toggle',
+                        options=[
+                            {'label': 'Counts', 'value': 'count'},
+                            {'label': 'Proportional', 'value': 'proportion'}
+                        ],
+                        value='proportion',
+                        labelStyle={'display': 'inline-block'}
+                    ),
                     dcc.Loading(html.Div(id='manager-event-container', children=[], style={'width': '100%'}), type='circle'),
                 ])
             ])
@@ -62,19 +71,94 @@ def render_content(tab):
         return driver_layout
 
 @callback(
-    Output('employee-dropdown', 'options'),
+    Output('manager-event-container', 'children'),
     Input('event-date-range-picker', 'start_date'),
-    Input('event-date-range-picker', 'end_date')
+    Input('event-date-range-picker', 'end_date'),
+    Input('manager-dropdown', 'value'),
+    Input('event-dropdown', 'value'),
+    Input('scale-toggle', 'value'),
+    prevent_initial_callback=True
 )
-def update_driver_options(start_date, end_date):
-    if not start_date or not end_date:
-        return []
-    df = fetch_employees_in_schedule_event(start_date, end_date)
-    df.sort_values(by='name', inplace=True)
-    driver_options = df.to_dict('records')
-    driver_options = [{'label': driver['name'], 'value': driver['id']} for driver in driver_options]
-    driver_options.insert(0, {'label': 'All drivers', 'value': 'all'})
-    return driver_options
+def render_manager_event_container(start_date, end_date, managers, events, scale):
+    if not managers:
+        return html.Div()
+    if 'all' in managers:
+        managers = None
+    df = fetch_driver_events_by_period_for_managers(start_date, end_date, managers=managers)
+    # df = df[~df['event'].isin(['Libre', 'Alta Medica'])]
+    df = expand_events(df)
+    df['week'] = df['date'].dt.isocalendar().week
+    df = df[(df['start'] >= start_date) & (df['end'] <= end_date)]
+    start_week = df['week'].min()
+    end_week = df['week'].max()
+    if events:
+        df = df[df['event'].isin(events)]
+    
+    total_col = df.groupby('week').size().reset_index(name='total_count')
+    dfg = df.groupby(['week', 'event']).agg({'event': 'count'}).rename(columns={'event': 'count'})
+    dfg.reset_index(inplace=True)
+    dfg = pd.merge(dfg, total_col, on=['week'], how='left')
+    dfg['proportion'] = dfg['count'] / dfg['total_count']
+    dfg['proportion'] = dfg['proportion'].apply(lambda x: round(x, 3))
+    pivot = dfg.pivot(index=['event'], columns=['week'], values=scale).fillna(0)
+    pivot = pivot.reset_index()
+    
+    if scale == 'count':
+        numeric_cols = pivot.select_dtypes(include='number').columns
+        pivot['Total'] = pivot[numeric_cols].sum(axis=1)
+
+    page_size = len(pivot)
+    grid = create_data_table('events-table', pivot, 'events.csv', page_size=page_size)
+
+    color_map = px.colors.qualitative.Plotly
+    event_colors = {event: color_map[i % len(color_map)] for i, event in enumerate(dfg['event'].unique())}
+
+    dfg = dfg.sort_values(by=scale, ascending=False)
+    histograms = []
+    for week in range(start_week, end_week + 1):
+        week_data = dfg[dfg['week'] == week]
+        hist_fig = px.bar(
+            week_data,
+            x=scale,
+            y='event', 
+            color='event',
+            title=f'Event Distribution for Week {week}',
+            color_discrete_map=event_colors,
+            orientation='h'
+        )
+        hist_fig.update_layout(xaxis_type="log")
+        hist_fig.update_layout(showlegend=False)
+        hist_fig.update_layout(yaxis_title="Events")
+        if scale == 'proportion':
+            hist_fig.update_layout(xaxis_title="Proportion")
+            hist_fig.update_layout(xaxis_tickformat=".1%")
+            hist_fig.update_layout(xaxis_tickangle=-45)
+        histograms.append(dbc.Col(dcc.Graph(figure=hist_fig), width=6))
+
+    global_hist_fig = px.bar(
+        dfg,
+        x=scale,
+        y='event',
+        color='event',
+        title=f'Global Event Distribution between {start_date} and {end_date}',
+        color_discrete_map=event_colors,
+        orientation='h'
+    )
+    global_hist_fig.update_layout(xaxis_type="log")
+    global_hist_fig.update_layout(showlegend=False)
+    if scale == 'count':
+        global_hist_fig.update_layout(yaxis_title="Event Count")
+    if scale == 'proportion':
+        global_hist_fig.update_layout(xaxis_title="Proportion")
+        global_hist_fig.update_layout(xaxis_tickformat=".1%")
+
+    histograms.append(dbc.Col(dcc.Graph(figure=global_hist_fig), width=12))
+
+    return [
+        dbc.Row(histograms),
+        grid
+    ]
+
 
 @callback(
     Output('driver-event-container', 'children'),
@@ -119,38 +203,45 @@ def render_driver_event_container(start_date, end_date, drivers, events, scale):
     color_map = px.colors.qualitative.Plotly
     event_colors = {event: color_map[i % len(color_map)] for i, event in enumerate(dfg['event'].unique())}
 
+    dfg = dfg.sort_values(by=scale, ascending=False)
     histograms = []
     for week in range(start_week, end_week + 1):
         week_data = dfg[dfg['week'] == week]
         hist_fig = px.bar(
             week_data,
-            x='event',
-            y=scale, 
+            x=scale,
+            y='event', 
             color='event',
             title=f'Event Distribution for Week {week}',
-            color_discrete_map=event_colors
+            color_discrete_map=event_colors,
+            orientation='h'
         )
-        hist_fig.update_layout(yaxis_type="log", showlegend=False)
-        hist_fig.update_layout(yaxis_title="Event Count")
+        hist_fig.update_layout(showlegend=False)
+        hist_fig.update_layout(yaxis_title="Events")
+        hist_fig.update_layout(xaxis_type="log")
         if scale == 'proportion':
-            hist_fig.update_layout(yaxis_title="Proportion")
-            hist_fig.update_layout(yaxis_tickformat=".1%")
+            hist_fig.update_layout(xaxis_title="Proportion")
+            hist_fig.update_layout(xaxis_tickformat=".1%")
+            hist_fig.update_layout(xaxis_tickangle=-45)
         histograms.append(dbc.Col(dcc.Graph(figure=hist_fig), width=6))
 
     global_hist_fig = px.bar(
         dfg,
-        x='event',
-        y=scale,
+        x=scale,
+        y='event',
         color='event',
         title=f'Global Event Distribution between {start_date} and {end_date}',
-        color_discrete_map=event_colors
+        color_discrete_map=event_colors,
+        orientation='h'
     )
+    global_hist_fig.update_layout(xaxis_type="log")
+    global_hist_fig.update_layout(xaxis_tickformat=".1%")
     if scale == 'count':
         global_hist_fig.update_layout(yaxis_type="log", showlegend=False)
-    global_hist_fig.update_layout(yaxis_title="Event Count")
+        global_hist_fig.update_layout(yaxis_title="Event Count")
     if scale == 'proportion':
-        global_hist_fig.update_layout(yaxis_type="log", showlegend=False)
-        global_hist_fig.update_layout(yaxis_title="Proportion")
+        global_hist_fig.update_layout(xaxis_type="log", showlegend=False)
+        global_hist_fig.update_layout(xaxis_title="Proportion")
         global_hist_fig.update_layout(yaxis_tickformat=".1%")
 
     histograms.append(dbc.Col(dcc.Graph(figure=global_hist_fig), width=12))
@@ -160,71 +251,17 @@ def render_driver_event_container(start_date, end_date, drivers, events, scale):
         grid
     ]
 
-
 @callback(
-    Output('manager-event-container', 'children'),
+    Output('employee-dropdown', 'options'),
     Input('event-date-range-picker', 'start_date'),
-    Input('event-date-range-picker', 'end_date'),
-    Input('manager-dropdown', 'value'),
-    Input('event-dropdown', 'value'),
-    prevent_initial_callback=True
+    Input('event-date-range-picker', 'end_date')
 )
-def render_manager_event_container(start_date, end_date, managers, events):
-    if not managers:
-        return html.Div()
-    df = fetch_driver_events_by_period_for_managers(start_date, end_date, managers=managers)
-    df = df[~df['event'].isin(['Libre', 'Alta Medica'])]
-    df = expand_events(df)
-    df['week'] = df['date'].dt.isocalendar().week
-    df = df[(df['start'] >= start_date) & (df['end'] <= end_date)]
-    start_week = df['week'].min()
-    end_week = df['week'].max()
-    if events:
-        df = df[df['event'].isin(events)]
-    dfg = df.groupby(['week', 'event']).agg({'event': 'count'}).rename(columns={'event': 'event_count'})
-    dfg.reset_index(inplace=True)
-    pivot = dfg.pivot(index=['event'], columns=['week'], values='event_count').fillna(0)
-    pivot = pivot.reset_index()
-    pivot = pivot.sort_values(by=['event'])
-    
-    numeric_cols = pivot.select_dtypes(include='number').columns
-    pivot['Total'] = pivot[numeric_cols].sum(axis=1)
-    
-    grid = create_data_table('events-table', pivot, 'events.csv')
-    
-    color_map = px.colors.qualitative.Plotly
-    event_colors = {event: color_map[i % len(color_map)] for i, event in enumerate(dfg['event'].unique())}
-    
-    line_fig = px.line(
-        dfg,
-        x='week',
-        y='event_count',
-        color='event',
-        title=f'Event Count from week {start_week} to {end_week}',
-        color_discrete_map=event_colors
-    )
-
-    line_fig.update_layout(
-        xaxis_title="Week",
-        yaxis_title="Event Count",
-        yaxis_type="log",
-        xaxis_tickangle=-45,
-        showlegend=False,
-        xaxis_rangeslider_visible=True
-    )
-
-    pie_fig = px.pie(
-        dfg,
-        values='event_count',
-        names='event',
-        color='event',
-        color_discrete_map=event_colors
-    )
-
-    return [
-        dbc.Row([
-            dbc.Col(dcc.Graph(figure=line_fig), width=6),
-            dbc.Col(dcc.Graph(figure=pie_fig), width=6)
-        ]),
-        grid
-    ]
+def update_driver_options(start_date, end_date):
+    if not start_date or not end_date:
+        return []
+    df = fetch_employees_in_schedule_event(start_date, end_date)
+    df.sort_values(by='name', inplace=True)
+    driver_options = df.to_dict('records')
+    driver_options = [{'label': driver['name'], 'value': driver['id']} for driver in driver_options]
+    driver_options.insert(0, {'label': 'All drivers', 'value': 'all'})
+    return driver_options
