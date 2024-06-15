fbd08ebf9f8d755b80884674f5932ccd73c9b7b5
ctlbau <ctrebbau@pm.me>
2024-06-15 19:47:07 +0200

    Better Bar charts for manager view of events

--- pages/events.py
+++ pages/events.py
@@ -1,6 +1,6 @@
 import dash
 from dash import callback, html, dcc
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
@@ -25,22 +25,24 @@ def render_content(tab):
         min_date, max_date = get_min_max_dates_from_schedule_events()
         manager_options = fetch_managers().to_dict('records')
         event_options = fetch_event_options().to_dict('records')
-        # event_options = [event for event in event_options if event['name'] not in ['Libre', 'Alta Medica']]
         manager_layout = dbc.Container([
             dbc.Row([
                 dbc.Col([
                     create_date_range_picker('event-date-range-picker', min_date, max_date),
                     create_dropdown('manager-dropdown', options=manager_options, label='name', value='name', placeholder='Select manager', multi=True, add_all=True),
                     create_dropdown('event-dropdown', options=event_options, label='name', value='name', placeholder='Select event', multi=True, add_all=False),
-                    dcc.RadioItems(
-                        id='scale-toggle',
-                        options=[
-                            {'label': 'Counts', 'value': 'count'},
-                            {'label': 'Proportional', 'value': 'proportion'}
-                        ],
-                        value='count',
-                        labelStyle={'display': 'inline-block'}
-                    ),
+                    dbc.Row([
+                        dbc.Col(dcc.RadioItems(
+                            id='scale-toggle',
+                            options=[
+                                {'label': 'Counts', 'value': 'count'},
+                                {'label': 'Proportional', 'value': 'proportion'}
+                            ],
+                            value='count',
+                            labelStyle={'display': 'inline-block'}
+                        ), className="col-md-4 offset-md-4 col-12"),
+                        dbc.Col(html.Button("Submit", id="manager-submit-button"), className="col-md-4 offset-md-4 col-12")
+                    ], className="mb-3"),
                     dcc.Loading(html.Div(id='manager-event-container', children=[], style={'width': '100%'}), type='circle'),
                 ])
             ])
@@ -62,7 +64,8 @@ def render_content(tab):
                             {'label': 'Proportional', 'value': 'proportion'}
                         ],
                         value='count',
-                        labelStyle={'display': 'inline-block'}
+                        labelStyle={'display': 'inline-block'},
+                        className="col-md-4 offset-md-4 col-12"
                     ),
                     dcc.Loading(html.Div(id='driver-event-container', children=[], style={'width': '100%'}), type='circle'),
                 ])
@@ -72,97 +75,62 @@ def render_content(tab):
 
 @callback(
     Output('manager-event-container', 'children'),
-    Input('event-date-range-picker', 'start_date'),
-    Input('event-date-range-picker', 'end_date'),
-    Input('manager-dropdown', 'value'),
-    Input('event-dropdown', 'value'),
-    Input('scale-toggle', 'value'),
+    State('event-date-range-picker', 'start_date'),
+    State('event-date-range-picker', 'end_date'),
+    State('manager-dropdown', 'value'),
+    State('event-dropdown', 'value'),
+    State('scale-toggle', 'value'),
+    Input('manager-submit-button', 'n_clicks'),
     prevent_initial_callback=True
 )
-def render_manager_event_container(start_date, end_date, managers, events, scale):
+def render_manager_event_container(start_date, end_date, managers, events, scale, n_clicks):
     if not managers:
         return html.Div()
     if 'all' in managers:
         managers = None
-    df = fetch_driver_events_by_period_for_managers(start_date, end_date, managers=managers)
-    # df = df[~df['event'].isin(['Libre', 'Alta Medica'])]
+    if n_clicks is None:
+        return html.Div()
+    df = fetch_driver_events_by_period_for_managers(start_date, end_date, managers)
+    if df.empty:
+        return html.Div()
+    if events:
+        df = df[df['event'].isin(events)]
     df = expand_events(df)
     df['week'] = df['date'].dt.isocalendar().week
     df = df[(df['start'] >= start_date) & (df['end'] <= end_date)]
-    start_week = df['week'].min()
-    end_week = df['week'].max()
-    if events:
-        df = df[df['event'].isin(events)]
-    
-    total_col = df.groupby('week').size().reset_index(name='total_count')
-    dfg = df.groupby(['week', 'event']).agg({'event': 'count'}).rename(columns={'event': 'count'})
-    dfg.reset_index(inplace=True)
-    dfg = pd.merge(dfg, total_col, on=['week'], how='left')
-    dfg['proportion'] = dfg['count'] / dfg['total_count']
+    total_event_col = df.groupby('manager').size().reset_index(name='total_manager_event_count')
+    df = df.merge(total_event_col, on='manager', how='left')
+    dfg = df.groupby(['manager', 'event']).agg({'event': 'count'}).rename(columns={'event': 'count'}).reset_index()
+    dfg = pd.merge(dfg, total_event_col, on=['manager'], how='left')
+    dfg['proportion'] = dfg['count'] / dfg['total_manager_event_count']
     dfg['proportion'] = dfg['proportion'].apply(lambda x: round(x, 3))
     dfg = dfg.sort_values(by=scale, ascending=False)
-    pivot = dfg.pivot(index=['event'], columns=['week'], values=scale).fillna(0)
-    pivot = pivot.reset_index()
     
-    if scale == 'count':
-        numeric_cols = pivot.select_dtypes(include='number').columns
-        pivot['Total'] = pivot[numeric_cols].sum(axis=1)
-
-    page_size = len(pivot)
-    grid = create_data_table('events-table', pivot, 'events.csv', page_size=page_size)
-
-    color_map = px.colors.qualitative.Plotly
-    event_colors = {event: color_map[i % len(color_map)] for i, event in enumerate(dfg['event'].unique())}
-
-    dfg = dfg.sort_values(by=scale, ascending=False)
-    bars = []
-    for week in range(start_week, end_week + 1):
-        week_data = dfg[dfg['week'] == week]
+    manager_bars = []
+    managers = dfg['manager'].unique()
+    for manager in managers:
+        manager_data = dfg[dfg['manager'] == manager]
+        n_drivers = df[df['manager'] == manager]['employee'].nunique()
         bar_fig = px.bar(
-            week_data,
+            manager_data,
             x=scale,
             y='event', 
             color='event',
-            title=f'Event Distribution for Week {week}',
-            color_discrete_map=event_colors,
+            title=f'{manager}<br><sub>{n_drivers} drivers between {start_date} and {end_date}</sub>',
             orientation='h'
         )
         bar_fig.update_layout(showlegend=False)
-        bar_fig.update_layout(yaxis_title="Events")
+        bar_fig.update_layout(xaxis_type="log")
         if scale == 'proportion':
-            bar_fig.update_layout(xaxis_title="Proportion")
             bar_fig.update_layout(xaxis_tickformat=".1%")
             bar_fig.update_layout(xaxis_tickangle=-45)
-        bars.append(dbc.Col(dcc.Graph(figure=bar_fig), width=6))
-
-    global_data = df.groupby('event').agg({'event': 'count'}).rename(columns={'event': 'count'})
-    global_data['proportion'] = global_data['count'] / global_data['count'].sum()
-    global_data['proportion'] = global_data['proportion'].apply(lambda x: round(x, 3))
-    global_data = global_data.reset_index()
-    global_data = global_data.sort_values(by=scale, ascending=False)
-    global_bar_fig = px.bar(
-        global_data,
-        x=scale,
-        y='event',
-        color='event',
-        title=f'Global Event Distribution between {start_date} and {end_date}',
-        color_discrete_map=event_colors,
-        orientation='h'
-    )
-    global_bar_fig.update_layout(showlegend=False)
-    global_bar_fig.update_layout(xaxis_type="log")
-    if scale == 'count':
-        global_bar_fig.update_layout(yaxis_title="Event Count")
-    if scale == 'proportion':
-        global_bar_fig.update_layout(xaxis_title="Event Proportion")
-        global_bar_fig.update_layout(xaxis_tickformat=".1%")
-        global_bar_fig.update_layout(xaxis_tickangle=-45)
-
-    bars.append(dbc.Col(dcc.Graph(figure=global_bar_fig), width=12))
-
+        manager_bars.append((n_drivers, dbc.Col(dcc.Graph(figure=bar_fig), width=6)))
+    
+    manager_bars.sort(key=lambda x: x[0], reverse=True)
+    sorted_bars = [bar for _, bar in manager_bars]
+    
     return dcc.Loading([
-        dbc.Row(bars),
-        grid
+        dbc.Row(sorted_bars),
     ], type='circle')
 
 
